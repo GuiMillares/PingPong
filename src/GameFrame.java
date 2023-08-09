cfdf3dd9e9470df1a0c91e1383ba361b2a5a7655
Guilherme Millares <guilhermec.millares@gmail.com>
2023-08-08 23:03:02 -0300

    Creating constructors and classes for the game of ping pong

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
-public class GameFrame {
+public class GameFrame extends JFrame{
 
+	GamePanel panel = new GamePanel();
+	
+	GameFrame(){
+		
+	}
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,56 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
-public class GamePanel {
-
+public class GamePanel extends JPanel implements Runnable{
+	
+	static final int GAME_WIDTH = 1000;
+	static final int GAME_HEIGHT = (int)(GAME_WIDTH * (5/9));
+	static final Dimension SCREEN_SIZE =new Dimension(GAME_WIDTH,GAME_HEIGHT);
+	static final int BALL_DIAMETER = 20;
+	static final int PADDLE_WIDTH = 25;
+	static final int PADDLE_HEIGHT = 100;
+	Thread gameThread;
+	Image image;
+	Graphics graphics;
+	Random random;
+	Paddle paddle1;
+	Paddle paddle2;
+	Ball ball;
+	Score score;
+	
+	GamePanel(){
+		
+	}
+	
+	public void newBall(){
+		
+	}
+	public void newPaddles() {
+		
+	}
+	public void paint(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move() {
+		
+	}
+	public void checkCollision() {
+		
+	}
+	public void run() {
+		
+	}
+	public class AL extends KeyAdapter{
+		public void kewPressed(KeyEvent e) {
+			
+		}
+		public void kewReleased(KeyEvent e) {
+			
+		}
+	}
 }
